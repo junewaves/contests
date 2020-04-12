5c8f3160729cabccb0fac4600525e6ec2b159a4c
Sam <q36084191@gs.ncku.edu.tw>
2020-04-12 21:26:58 +0800

    Codeforces Round 633 prep

---
+++ Codeforces/633/pE.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long int ll;
+void solve() {
+    cout << endl;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int _n;
+    cin >> _n;
+    while (_n--) {
+        solve();
+    }
+    return 0;
+}
