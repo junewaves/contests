c95cd598d21036ca12f9bd874b1e5fcc1c743198
junewaves <a0929551932@gmail.com>
2020-04-14 03:57:42 +0800

    Leetcode Contiguous array (practice)

--- practice_and_test/leetcode_contiguous.cpp
+++ practice_and_test/leetcode_contiguous.cpp
@@ -1,37 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// int devide(vector<int>& s, int l, int r) {
-//     if (r - l <= 1)
-//         return 0;
-//     int m = (l + r) / 2;
-//     int left = devide(s, l, m);
-//     int right = devide(s, m, r);
-//     int m1 = m + 1, m2 = m - 1;
-//     int len = 0;
-//     while (m1 <= (int)s.size() && m2 >= 0) {
-//         if ((s[m1] - s[m2]) * 2 == m1 - m2) {
-//             len = m1 - m2;
-//         }
-//         m1++, m2--;
-//     }
-//     return max({ len, right, left });
-// }
-
 int findMaxLength(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> s(n + 1);
-    s[0] = 0;
-    for (int i = 1; i <= n; i++) {
-        s[i] = nums[i - 1] + s[i - 1];
-    }
-    int max_len = 0;
-    for (int i = 0; i <= n - max_len; i++) {
-        for (int j = i + max_len + 2; j <= n; j += 2) {
-            if ((s[j] - s[i]) * 2 == j - i) {
-                max_len = max(max_len, j - i);
-            }
-        }
+    int n = nums.size(), s = 0, max_len = 0;
+    unordered_map<int, int> m;
+    m[0] = -1;
+    for (int i = 0; i < n; i++) {
+        s += nums[i] ? 1 : -1;
+        if (m.count(s))
+            max_len = max(max_len, i - m[s]);
+        else
+            m[s] = i;
     }
     return max_len;
 }
