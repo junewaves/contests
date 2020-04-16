d5d6123dd9f2709aff9eb27179f8f1344e42e9ff
junewaves <a0929551932@gmail.com>
2020-04-17 03:30:06 +0800

    test

--- practice_and_test/UVa10405_LCS.cpp
+++ practice_and_test/UVa10405_LCS.cpp
@@ -6,14 +6,14 @@ int main() {
     cin.tie(0);
     string s1, s2;
     while (getline(cin, s1) && getline(cin, s2)) {
-        int n1 = s1.size(), n2 = s2.size();
-        if (n1 == 0 || n2 == 0) {
+        int size1 = s1.size(), size2 = s2.size();
+        if (size1 == 0 || size2 == 0) {
             cout << 0 << endl;
             continue;
         }
-        vector<vector<int>> lcs(n1, vector<int>(n2, 0));
-        for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < n2; j++) {
+        vector<vector<int>> lcs(size1, vector<int>(size2, 0));
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++) {
                 if (s1[i] == s2[j]) {
                     if (i == 0 || j == 0)
                         lcs[i][j] = 1;
@@ -26,25 +26,25 @@ int main() {
                 }
             }
         }
-        cout << lcs[n1 - 1][n2 - 1] << endl;
-        // int i = n1 - 1, j = n2 - 1, k = lcs[n1 - 1][n2 - 1];
-        // cout << k << endl;
-        // vector<char> ans(k);
-        // while (i >= 0 && j >= 0) {
-        //     int up = i == 0 ? 0 : lcs[i - 1][j];
-        //     int left = j == 0 ? 0 : lcs[i][j - 1];
-        //     if (lcs[i][j] > max(up, left)) {
-        //         ans[--k] = s1[i];
-        //         i--;
-        //         j--;
-        //     } else if (lcs[i][j] == up) {
-        //         i--;
-        //     } else {
-        //         j--;
-        //     }
-        // }
-        // for (char c : ans)
-        //     cout << c;
-        // cout << endl;
+        // cout << lcs[size1 - 1][size2 - 1] << endl;
+        int i = size1 - 1, j = size2 - 1, k = lcs[size1 - 1][size2 - 1];
+        cout << k << endl;
+        vector<char> ans(k);
+        while (i >= 0 && j >= 0) {
+            int up = i == 0 ? 0 : lcs[i - 1][j];
+            int left = j == 0 ? 0 : lcs[i][j - 1];
+            if (lcs[i][j] > max(up, left)) {
+                ans[--k] = s1[i];
+                i--;
+                j--;
+            } else if (lcs[i][j] == up) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+        for (char c : ans)
+            cout << c;
+        cout << endl;
     }
 }
