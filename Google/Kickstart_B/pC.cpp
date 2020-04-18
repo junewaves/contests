080523140db7e06f5d7e128f1af086f44e2af92a
junewaves <a0929551932@gmail.com>
2020-04-19 06:48:53 +0800

    Kickstart 2020 Round B prep.

---
+++ Google/Kickstart_B/pC.cpp
@@ -0,0 +1,18 @@
+// Kickstart 2020 Round B (4/19)
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef _VSCODE_DEBUG
+#define endl '\n'
+#endif
+typedef long long int ll;
+void solve() {}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int _n;
+    cin >> _n;
+    for (int i = 1; i <= _n; i++) {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+}
