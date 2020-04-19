bb590620029a8f9724a1afcc492e8187e7f9fb54
junewaves <a0929551932@gmail.com>
2020-04-19 23:59:51 +0800

    Code Jam Round 1B prep.

---
+++ Google/CodeJam_Round_1B/pT.cpp
@@ -0,0 +1,17 @@
+// Code Jam Round 1B (4/19)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long ll;
+void solve() {}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int _n;
+    cin >> _n;
+    for (int _cn = 1; _cn <= _n; _cn++) {
+        cout << "Case #" << _cn << ": ";
+        solve();
+    }
+    return 0;
+}
