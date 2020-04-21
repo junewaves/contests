b1329d7da2339f61fccf2104bdbb220ed051d861
junewaves <a0929551932@gmail.com>
2020-04-21 21:46:22 +0800

    Codeforces Round 636 prep.

---
+++ Codeforces/636/pD.cpp
@@ -0,0 +1,16 @@
+// Codeforces Round #636 (Div. 2) (4/21)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long int ll;
+void solve() {}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int _n;
+    cin >> _n;
+    while (_n--) {
+        solve();
+    }
+    return 0;
+}
