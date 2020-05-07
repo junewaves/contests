c4db9f0e1e0ec5fe7e9c4ec481d7c4848d6f2809
junewaves <a0929551932@gmail.com>
2020-05-07 23:29:34 +0800

    practice

--- NCKU_CP/Class_contest_4/p46.cpp
+++ NCKU_CP/Class_contest_4/p46.cpp
@@ -3,26 +3,20 @@
 #define endl '\n'
 #endif
 using namespace std;
+int n, k, s[26];
+char c;
 typedef long long ll;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n, k;
     cin >> n >> k;
-    unordered_map<char, int> hashing;
-    char c;
     for (int i = 0; i < n; i++) {
         cin >> c;
-        hashing[c] += 1;
+        s[c - 'A']++;
     }
-    if ((int)hashing.size() != k)
-        cout << 0 << endl;
-    else {
-        int lo = hashing['A'];
-        for (auto& e : hashing) {
-            lo = min(e.second, lo);
-        }
-        cout << lo * k << endl;
-    }
+    int a = 1000000;
+    for (int i = 0; i < k; i++)
+        a = min(a, s[i]);
+    cout << a * k << endl;
     return 0;
 }
--- NCKU_CP/Class_contest_4/p47.cpp
+++ NCKU_CP/Class_contest_4/p47.cpp
@@ -8,25 +8,20 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     int n;
-    cin >> n;
     string a, b;
-    cin >> a >> b;
-    vector<vector<int>> s(2, vector<int>(n + 1));
-    int d = 0;
-
+    cin >> n >> a >> b;
+    vector<int> c(n + 1), d(n + 1);
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             if (a[i - 1] == b[j - 1]) {
-                s[1][j] = s[0][j - 1] + 1;
+                d[j] = c[j - 1] + 1;
             } else {
-                s[1][j] = max(s[0][j], s[1][j - 1]);
+                d[j] = max(c[j], d[j - 1]);
             }
         }
-        for (int j = 0; j <= n; j++) {
-            s[0][j] = s[1][j];
-            s[1][j] = 0;
-        }
+        if (i != n)
+            copy(d.begin(), d.end(), c.begin());
     }
-    cout << s[0][n] << endl;
+    cout << d[n] << endl;
     return 0;
 }
