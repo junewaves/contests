b5afc189758463731de402d1d6c52a49edfca23a
junewaves <a0929551932@gmail.com>
2020-05-10 22:14:36 +0800

    C is just brute force :(

--- AtCoder/167/pC.cpp
+++ AtCoder/167/pC.cpp
@@ -7,17 +7,38 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     int n, m, x;
-    vector<vector<int>> c(n, vector<int>(m));
-    vector<int> ml(m, 0);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++) {
+    cin >> n >> m >> x;
+    vector<vector<int>> c(n, vector<int>(m + 1));
+    ll min_cost = LONG_LONG_MAX;
+    for (int i = 0; i < n; i++) {
+        cin >> c[i][0];
+        for (int j = 1; j <= m; j++)
             cin >> c[i][j];
-            ml[j] += c[i][j];
-        }
-    for (int l : ml) {
-        if (l < x) {
-            cout << "-1";
-            return 0;
-        }
     }
+    for (int bit = 1; bit < (1 << n); bit++) {
+        ll cost = 0;
+        vector<int> level(m + 1, 0);
+        for (int i = 0; i < n; i++) {
+            if (bit & (1 << i)) {
+                cost += c[i][0];
+                for (int j = 1; j <= m; j++)
+                    level[j] += c[i][j];
+            }
+        }
+        bool ok = true;
+        for (int j = 1; j <= m; j++) {
+            // cout << level[j] << " ";
+            if (level[j] < x) {
+                ok = false;
+                break;
+            }
+        }
+        // cout << endl;
+        if (ok)
+            min_cost = min(min_cost, cost);
+    }
+    if (min_cost == LONG_LONG_MAX)
+        cout << "-1";
+    else
+        cout << min_cost;
 }
