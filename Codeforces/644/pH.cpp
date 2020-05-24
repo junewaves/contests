7793ce044eaa12d04681fea331dc80103894f5d9
junewaves <a0929551932@gmail.com>
2020-05-25 03:41:31 +0800

    fix name

--- Codeforces/644/pH.cpp
+++ Codeforces/644/pH.cpp
@@ -13,31 +13,31 @@ void solve() {
     }
     sort(a.begin(), a.end());
 
-    ll med = ((1LL << m) - n - 1) / 2;  // index of the new median
-    ll l = 0LL, r = (1LL << m) - 1;
-    while (l < r) {
-        ll m = l + (r - l) / 2;
+    ll target = ((1LL << m) - n - 1) / 2;  // index of the new median
+    ll lo = 0LL, hi = (1LL << m) - 1;
+    while (lo < hi) {
+        ll mid = lo + (hi - lo) / 2;
         // new index of m = m - (# of removed numbers that is less than m)
-        ll idx = m - (lower_bound(a.begin(), a.end(), m) - a.begin());
-        if (idx >= med)
-            r = m;
+        ll idx = mid - (lower_bound(a.begin(), a.end(), mid) - a.begin());
+        if (idx >= target)
+            hi = mid;
         else
-            l = m + 1;
+            lo = mid + 1;
     }
     // +1 if the answer we found is also removed
-    auto it = lower_bound(a.begin(), a.end(), l);
-    while (it != a.end() && *it == l) {
-        l++, it++;
+    auto it = lower_bound(a.begin(), a.end(), lo);
+    while (it != a.end() && *it == lo) {
+        lo++, it++;
     }
     // vector<char> ans(m);
     // for (int i = m - 1; i >= 0; i--) {
-    //     ans[i] = '0' + (char)(l & 1LL);
-    //     l >>= 1;
+    //     ans[i] = '0' + (char)(lo & 1LL);
+    //     lo >>= 1;
     // }
     // for (char c : ans)
     //     cout << c;
     for (ll i = 1LL << (m - 1); i > 0; i >>= 1) {
-        cout << (l & i ? '1' : '0');
+        cout << (lo & i ? '1' : '0');
     }
     cout << '\n';
 }
