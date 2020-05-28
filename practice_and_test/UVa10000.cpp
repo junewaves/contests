8fc36b2c3271c580eb654cc354403febb33d4484
junewaves <a0929551932@gmail.com>
2020-05-28 21:29:19 +0800

    practice

--- practice_and_test/UVa10000.cpp
+++ practice_and_test/UVa10000.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 constexpr int INF = 2e9;
 
-struct Edge {
-    int u, v;
-    Edge(int a, int b) : u(a), v(b) {}
-};
+// struct Edge {  // for Bellman-Ford’s Algorithm
+//     int u, v;
+//     Edge(int a, int b) : u(a), v(b) {}
+// };
 
 bool solve() {
     static int cn = 0;
@@ -15,21 +15,42 @@ bool solve() {
     if (!n)
         return false;
     cin >> s;  // source point
-    vector<Edge> E;
+    // vector<Edge> E;  // for Bellman-Ford’s Algorithm
+    vector<vector<int>> adj(n + 1);  // for SPFA
     while (1) {
         cin >> x >> y;
         if (x == 0 && y == 0)
             break;
-        E.emplace_back(x, y);
+        // E.emplace_back(x, y);
+        adj[x].push_back(y);
     }
     vector<int> d(n + 1, -INF);
     d[s] = 0;
-
-    for (int i = 1; i < n; i++) {
-        for (const Edge& e : E) {
-            if (e.v == s)
-                continue;
-            d[e.v] = max(d[e.v], d[e.u] + 1);
+    //  Bellman-Ford’s Algorithm
+    // for (int i = 1; i < n; i++) {
+    //     for (const Edge& e : E) {
+    //         if (e.v == s)
+    //             continue;
+    //         d[e.v] = max(d[e.v], d[e.u] + 1);
+    //     }
+    // }
+    // SPFA
+    queue<int> q;
+    vector<bool> inq(n + 1, false);
+    q.push(s);
+    inq[s] = true;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        inq[u] = false;
+        for (int v : adj[u]) {
+            if (d[v] < d[u] + 1) {
+                d[v] = d[u] + 1;
+                if (!inq[v]) {
+                    q.push(v);
+                    inq[v] = true;
+                }
+            }
         }
     }
     int maxd = -INF, maxp = 0;
